df2ffb0906d1ac5bebae70b3b7661e9192776651
Crowdbotics <team@crowdbotics.com>
2022-01-29 20:51:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "blackmoon_exchange_33237",
+  url: "https://blackmoon_exchange_33237.botics.co",
+  api: "https://blackmoon_exchange_33237.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
